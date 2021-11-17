098bde7885dac2c60951fcbe2dde943958faea87
hemang <hemangahujaeotw@gmail.com>
2021-11-17 20:20:29 +0530

    flexible widget added + appbar color fixed

--- lib/app_bar.dart
+++ lib/app_bar.dart
@@ -11,24 +11,25 @@ class MusicAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      backgroundColor: Colors.white,
        title: Row(
          mainAxisAlignment: MainAxisAlignment.spaceBetween,
          children: [
           
-             Flexible( flex : 1 , child: IconButton(onPressed: ()=>{print('object')}, icon : Icon(appBarIcon))),
+             Flexible( flex : 1 , child: IconButton(onPressed: ()=>{}, icon : Icon(appBarIcon , color : Colors.black))),
              const Spacer(flex: 1,),
-             Flexible(flex : 9 , child: Text(appBarTitle , style: const TextStyle(fontSize: 20),)),
+             Flexible(flex : 9 , child: Text(appBarTitle , style: const TextStyle(fontSize: 20 , color : Colors.black),)),
              const Spacer(flex: 6,),
              Flexible(
                flex : 7,
                child: CircleAvatar(
-                 radius: 20,
+                 radius: 25,
                  backgroundImage: NetworkImage(
                    userImageURL
                  ),
                ),
              ),
-             Flexible( flex : 2 ,child:  IconButton(onPressed: ()=>{}, icon : const Icon(Icons.settings)))
+             Flexible( flex : 2 ,child:  IconButton(onPressed: ()=>{}, icon : const Icon(Icons.settings , color : Colors.black)))
          ],
        )
     );
