a371fa7534347908caa60189c7ba43233fce55fd
dtekr <tekrawaladishant@gmail.com>
2021-11-27 14:35:58 +0530

    added all extra text and aligned button

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -12,33 +12,29 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text(
-          "Appella",
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 30.0,
-            color: Colors.black,
-          ),
-        ),
         backgroundColor: Colors.white,
         centerTitle: true,
+        elevation: 0,
       ),
       body: Column(
         children: <Widget>[
-          Container(
-            padding: const EdgeInsets.only(top: 30.0),
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(250.0),
-            ),
-            child: Center(
-                child: Text(
+          Column(
+              children: const [
+                Text(
+                  "Musiko",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black,
+                      fontSize: 30.0),
+                ),
+                Text(
                   "Let's get started.",
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
-                      color: Colors.blue[900],
+                      color: Color.fromRGBO(37,22,133,1),
                       fontSize: 30.0),
-                )),
-          ),
+                ),
+              ]),
           Container(
               padding: const EdgeInsets.only(top: 20.0, bottom: 20.0),
               decoration: BoxDecoration(
@@ -79,18 +75,23 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
+          const Text("Username", style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(37,22,133,1)),),
           TextFormField(
             decoration: const InputDecoration(
-              hintText: 'Enter your email',
+              hintText: 'Your email',
+                prefixIcon: Icon(Icons.person, color: Color.fromRGBO(75,52,223,1),)
             ),
             validator: (String? value) {
               if (value==null||(value.isEmail()==false)) {
                 return 'Please enter valid email';
               }},
           ),
+          const Text("Password", style: TextStyle(fontWeight: FontWeight.bold, color: Color.fromRGBO(37,22,133,1)),),
           TextFormField(
             decoration: const InputDecoration(
-              hintText: 'Enter your pass',
+              hintText: 'Your pass',
+              prefixIcon: Icon(Icons.lock, color: Color.fromRGBO(75,52,223,1),),
+              suffixIcon: Icon(Icons.visibility_outlined,color: Color.fromRGBO(156,155,179,1),),
             ),
             obscureText: true,
             validator: (String? value) {
@@ -100,21 +101,40 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
               }},
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 16.0),
-            child: ElevatedButton(
-              onPressed: () {
-                // Validate will return true if the form is valid, or false if
-                // the form is invalid.
-                if (_formKey.currentState!.validate()) {
-                  // Process data.
-                  Navigator.pushAndRemoveUntil(
-                      context, MaterialPageRoute(builder: (_) => MyApp()), (r) => false);
-                }
-              },
-              child: const Text('Submit'),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: const [
+                Text("Forgot password",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,color: Color.fromRGBO(75,52,223,1)))
+              ]
             ),
-          ),
+            Column(
+              children: [
+                Center(
+                  child: ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all<Color>(const Color.fromRGBO(75,52,223,1)),
+                    ),
+                    onPressed: () {
+                      // Validate will return true if the form is valid, or false if
+                      // the form is invalid.
+                      if (_formKey.currentState!.validate()) {
+                        // Process data.
+                        Navigator.pushAndRemoveUntil(
+                            context, MaterialPageRoute(builder: (_) => MyApp()), (r) => false);
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: const [
+                        Text('Submit',style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),),
+                      ],
+                    ),
+                  ),
+                ),
+                const Text("or sign in with qr code",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 16,color: Color.fromRGBO(75,52,223,1))),
+              ],
+            ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -11,200 +11,204 @@ class MyApp extends StatelessWidget {
     final List<String> names = ['Hands To Myself', 'At My Worst', 'Sample', 'Music', 'Name'];
     return MaterialApp(
         title: 'Main',
-        home: Scaffold(
-            backgroundColor: Colors.white,
-            appBar: const MusicAppBar(
-                appBarIcon: Icons.arrow_upward_rounded,
-                appBarTitle: 'Upload',
-                userImageURL:
-                'https://img.etimg.com/thumb/msid-81525531,width-650,imgsize-622277,,resizemode-4,quality-100/music_thinkstockphotos.jpg'),
-            body: Column(
-              children: [
-                Expanded(
-                  flex:4,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 20, left:20, right:20),
-                    child: SingleChildScrollView(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.all(30),
-                              decoration: const BoxDecoration(
-                                color: Color(0xffF7F2EE),
-                                borderRadius: BorderRadius.all(Radius.circular(10)),
-                              ),
-                              child: IntrinsicHeight(
-                                child: Row(
-                                  children: [
-                                  Expanded(
-                                    flex :3,
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+        home: WillPopScope(
+
+          onWillPop: ()async => false,
+          child: Scaffold(
+              backgroundColor: Colors.white,
+              appBar: const MusicAppBar(
+                  appBarIcon: Icons.arrow_upward_rounded,
+                  appBarTitle: 'Upload',
+                  userImageURL:
+                  'https://img.etimg.com/thumb/msid-81525531,width-650,imgsize-622277,,resizemode-4,quality-100/music_thinkstockphotos.jpg'),
+              body: Column(
+                children: [
+                  Expanded(
+                    flex:4,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 20, left:20, right:20),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: EdgeInsets.all(30),
+                                decoration: const BoxDecoration(
+                                  color: Color(0xffF7F2EE),
+                                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                                ),
+                                child: IntrinsicHeight(
+                                  child: Row(
                                     children: [
-                                      const Text(
-                                        'New Album / Song',
-                                        style: TextStyle(
-                                            fontSize: 18, fontWeight: FontWeight.w900),
-                                      ),
-                                      const Text(
-                                        'Upload any song on cloud and share on social',
-                                        maxLines: 3,
-                                        style: TextStyle(
-                                          fontSize: 15,),
-                                      ),
-                                      Container(
-                                        width: 80,
-                                        padding: const EdgeInsets.all(10),
-                                        decoration: const BoxDecoration(
-                                          color: Color(0xffFF7849),
-                                          borderRadius: BorderRadius.all(Radius.circular(3)),
+                                    Expanded(
+                                      flex :3,
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                      children: [
+                                        const Text(
+                                          'New Album / Song',
+                                          style: TextStyle(
+                                              fontSize: 18, fontWeight: FontWeight.w900),
                                         ),
-                                        child: Row(
-                                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                          children: const [
-                                            Icon(Icons.upload,color: Colors.white,size: 13,),
-                                            Text( 'Upload', style: TextStyle(fontSize: 13, color: Colors.white,
-                                        ),), ],
+                                        const Text(
+                                          'Upload any song on cloud and share on social',
+                                          maxLines: 3,
+                                          style: TextStyle(
+                                            fontSize: 15,),
                                         ),
-                                      ),
-                                        ],
-                                      ),
+                                        Container(
+                                          width: 80,
+                                          padding: const EdgeInsets.all(10),
+                                          decoration: const BoxDecoration(
+                                            color: Color(0xffFF7849),
+                                            borderRadius: BorderRadius.all(Radius.circular(3)),
+                                          ),
+                                          child: Row(
+                                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                            children: const [
+                                              Icon(Icons.upload,color: Colors.white,size: 13,),
+                                              Text( 'Upload', style: TextStyle(fontSize: 13, color: Colors.white,
+                                          ),), ],
+                                          ),
+                                        ),
+                                          ],
+                                        ),
+                                    ),
+                                      Expanded(flex: 2,child: Image.asset('images/img.png'))
+                                    ],
                                   ),
-                                    Expanded(flex: 2,child: Image.asset('images/img.png'))
-                                  ],
                                 ),
                               ),
-                            ),
-                            const SizedBox(height: 40,),
-                            const Text(
-                              'In Progress',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.w900),
-                            ),
-                            for ( int i = 0; i < names.length; i++ )
-                              Padding(
-                                padding: const EdgeInsets.symmetric(vertical : 8.0),
-                                child: Column(
-                                  children: [
-                                    TrackCard(data: TrackInfo(
-                                      name: names[i],
-                                      imageURL: 'https://img.etimg.com/thumb/msid-81525531,width-650,imgsize-622277,,resizemode-4,quality-100/music_thinkstockphotos.jpg',
-                                      releaseDate: DateTime(2002),
-                                      length: const Duration(seconds: 10),
-                                      price: 5,
-                                    ) ),
-                                    Divider(thickness: 2,)
-                                  ],
-                                ),
-                              )
-                          ]),
+                              const SizedBox(height: 40,),
+                              const Text(
+                                'In Progress',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w900),
+                              ),
+                              for ( int i = 0; i < names.length; i++ )
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(vertical : 8.0),
+                                  child: Column(
+                                    children: [
+                                      TrackCard(data: TrackInfo(
+                                        name: names[i],
+                                        imageURL: 'https://img.etimg.com/thumb/msid-81525531,width-650,imgsize-622277,,resizemode-4,quality-100/music_thinkstockphotos.jpg',
+                                        releaseDate: DateTime(2002),
+                                        length: const Duration(seconds: 10),
+                                        price: 5,
+                                      ) ),
+                                      Divider(thickness: 2,)
+                                    ],
+                                  ),
+                                )
+                            ]),
+                      ),
                     ),
                   ),
-                ),
-                Column(
-                  children: [
-                    Container(
-                      padding: const EdgeInsets.all(20),
-                      decoration: const BoxDecoration(
-                        color: Color(0xffF7F2EE),
-                        borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(30),
-                            topLeft: Radius.circular(30)),
-                      ),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const Text(
-                            'My Tracks (35)',
-                            style: TextStyle(
-                                fontSize: 20, fontWeight: FontWeight.w900),
-                          ),
-                          Container(
-                            padding: const EdgeInsets.all(8),
-                            child: const Text(
-                              'See All',
-                              style: TextStyle(
-                                  fontSize: 12, fontWeight: FontWeight.bold),
-                            ),
-                            decoration: BoxDecoration(
-                              border: Border.all(width: 1),
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                    Container(
-                      color: const Color(0xffF7F2EE),
-                      child: Container(
-                        height: 90,
+                  Column(
+                    children: [
+                      Container(
+                        padding: const EdgeInsets.all(20),
                         decoration: const BoxDecoration(
-                          color: Colors.white,
+                          color: Color(0xffF7F2EE),
                           borderRadius: BorderRadius.only(
                               topRight: Radius.circular(30),
                               topLeft: Radius.circular(30)),
                         ),
                         child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            const Icon(
-                              Icons.home_outlined,
-                              color: Colors.grey,
-                              size: 32,
-                            ),
-                            const Icon(
-                              Icons.search,
-                              color: Colors.grey,
-                              size: 32,
+                            const Text(
+                              'My Tracks (35)',
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.w900),
                             ),
                             Container(
-                              width: 65,
-                              //padding: const EdgeInsets.all(10.0),
-                              alignment: Alignment.center,
-                              decoration: const BoxDecoration(
-                                color: Color.fromRGBO(255, 212, 91, 1), // Colors(0xffRRGGBB)
-                                shape: BoxShape.circle,
+                              padding: const EdgeInsets.all(8),
+                              child: const Text(
+                                'See All',
+                                style: TextStyle(
+                                    fontSize: 12, fontWeight: FontWeight.bold),
                               ),
-                              child: const Icon(
-                                Icons.add,
-                                color: Colors.black,
-                                size: 25,
-                              ),
-                            ),
-                            const Icon(
-                              Icons.library_music_outlined,
-                              color: Colors.grey,
-                              size: 32,
-                            ),
-                            Transform.translate(
-                              offset: Offset(0,13),
-                              child: Column(
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                mainAxisSize: MainAxisSize.min,
-                                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                                children: const [
-                                  Text(
-                                    "Upload",
-                                    textAlign: TextAlign.start,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold, fontSize: 15),
-                                  ),
-                                  Icon(
-                                    Icons.horizontal_rule_outlined,
-                                    color: Colors.deepOrange,
-                                  )
-                                ],
+                              decoration: BoxDecoration(
+                                border: Border.all(width: 1),
+                                borderRadius: BorderRadius.circular(5),
                               ),
                             )
                           ],
                         ),
                       ),
-                    ),
-                  ],
-                )
-              ],
-            )));
+                      Container(
+                        color: const Color(0xffF7F2EE),
+                        child: Container(
+                          height: 90,
+                          decoration: const BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.only(
+                                topRight: Radius.circular(30),
+                                topLeft: Radius.circular(30)),
+                          ),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              const Icon(
+                                Icons.home_outlined,
+                                color: Colors.grey,
+                                size: 32,
+                              ),
+                              const Icon(
+                                Icons.search,
+                                color: Colors.grey,
+                                size: 32,
+                              ),
+                              Container(
+                                width: 65,
+                                //padding: const EdgeInsets.all(10.0),
+                                alignment: Alignment.center,
+                                decoration: const BoxDecoration(
+                                  color: Color.fromRGBO(255, 212, 91, 1), // Colors(0xffRRGGBB)
+                                  shape: BoxShape.circle,
+                                ),
+                                child: const Icon(
+                                  Icons.add,
+                                  color: Colors.black,
+                                  size: 25,
+                                ),
+                              ),
+                              const Icon(
+                                Icons.library_music_outlined,
+                                color: Colors.grey,
+                                size: 32,
+                              ),
+                              Transform.translate(
+                                offset: Offset(0,13),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  mainAxisSize: MainAxisSize.min,
+                                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                                  children: const [
+                                    Text(
+                                      "Upload",
+                                      textAlign: TextAlign.start,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold, fontSize: 15),
+                                    ),
+                                    Icon(
+                                      Icons.horizontal_rule_outlined,
+                                      color: Colors.deepOrange,
+                                    )
+                                  ],
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              )),
+        ));
   }
 }
